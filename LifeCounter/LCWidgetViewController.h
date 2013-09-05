e6680e216f446c5ff8fbda07feb9ca51fff25361
Wylie Conlon <wylieconlon@gmail.com>
2013-09-05 16:34:58 -0400

    Initial commit

---
+++ LifeCounter/LCWidgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCWidgetViewController.h
+//  LifeCounter
+//
+//  Created by Wylie on 9/5/13.
+//  Copyright (c) 2013 Wylie Conlon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCWidgetViewController : UIViewController
+
+@end
---
+++ LifeCounter/LCWidgetViewController.m
@@ -0,0 +1,38 @@
+//
+//  LCWidgetViewController.m
+//  LifeCounter
+//
+//  Created by Wylie on 9/5/13.
+//  Copyright (c) 2013 Wylie Conlon. All rights reserved.
+//
+
+#import "LCWidgetViewController.h"
+
+@interface LCWidgetViewController ()
+
+@end
+
+@implementation LCWidgetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
